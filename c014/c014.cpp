e25ac548eb619cfbb33f277e4a7d7e5bb8f7513b
QAQgettingstupid <venustsai211@gmail.com>
2024-01-07 17:20:24 +0800

    add a737

--- c014/c014.cpp
+++ c014/c014.cpp
@@ -1,20 +1,43 @@
-﻿// c014.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <cmath>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string up, down;
+    while (cin >> up) {
+        cin >> down;
+        int count = 0, carry = 0, num[3][11] = { 0 };
+        if (up == down && down == "0")
+            break;
+        for (int i = 0; i < up.size(); i++) {
+            num[0][i] = up[up.size() - 1 - i] - '0';
+        }
+        for (int i = 0; i < down.size(); i++) {
+            num[1][i] = down[down.size() - 1 - i] - '0';
+        }
+        int i = 0,add;
+        while (num[0][i] || num[1][i] || i<max(up.size(),down.size())) {
+            add = num[0][i] + num[1][i] + carry;
+            if (add >= 10) {
+                count++;
+                carry = 1;
+            }
+            else
+                carry = 0;
+            i++;
+        }
+        if (count > 1)
+            cout << count << " carry operations.\n";
+        else if (count)
+            cout << "1 carry operation.\n";
+        else
+            cout << "No carry operation.\n";
+
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
