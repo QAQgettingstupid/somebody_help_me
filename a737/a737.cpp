b51a0d635bb559abc9fc2d5315aa625f5d6c88b4
QAQgettingstupid <venustsai211@gmail.com>
2024-01-08 15:57:51 +0800

    add a737

--- a737/a737.cpp
+++ a737/a737.cpp
@@ -1,20 +1,23 @@
-﻿// a737.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <algorithm>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n, house, r;
+    int s[510];
+    cin >> n;
+    while (n--) {
+        cin >> r;
+        int sum = 0;
+        for (int i = 0; i < r; i++)
+            cin >> s[i];
+        sort(s, s + r);
+        house = s[(r-1) / 2];
+        for (int i = 0; i < r; i++)
+            sum += abs(house - s[i]);
+        cout << sum << '\n';
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
