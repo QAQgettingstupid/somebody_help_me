ea01eb260f3e7e72e972d464b85e24bff81a7b2a
QAQgettingstupid <venustsai211@gmail.com>
2024-01-10 20:02:54 +0800

    幹 物件導向是三小

---
+++ oop/oop.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class oop{
+
+public:
+
+    oop(int y, int m, int d) : year(y), month(m), date(d) {
+
+    }
+    void print() {
+        cout << year << "/" << month << "/" << date << '\n';
+    }
+    void set(int y,int m,int d){
+        year = y;
+        month = m;
+        date = d;
+    }
+
+private:
+    int year;
+    int month;
+    int date;
+    
+};
+
+int main()
+{
+    
+    oop f1(2024,1,10);
+    f1.print();
+
+    f1.set(2024,1,11);
+
+    f1.print();
+    
+    return 0;
+}
