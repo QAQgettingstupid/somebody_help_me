230434b93bf31bdc42c768cf50a4e38cea4d8b86
QAQgettingstupid <venustsai211@gmail.com>
2024-01-10 13:45:09 +0800

    add e579

--- e579/e579.cpp
+++ e579/e579.cpp
@@ -18,12 +18,17 @@ int main()
             v.push_back(i);
         }
         sort(v.begin(), v.end());
-        if (v[0] == 1)
-            cout << N << '\n';
+        int count = 0;
+        if (v[0] == 1) {
+            count = N - (N / 7 * 2);
+            if (N % 7 == 6)
+                count--;
+            cout << count << '\n';
+        }
         //削減因數
         else {
             for (int j = v.size() - 1; j > 0; j--) {
-                for (int k = j; k >= 0; k--) {
+                for (int k = j-1; k >= 0; k--) {
                     if (!(v[j] % v[k])) {
                         v.erase(v.begin() + j);
                         break;
@@ -31,7 +36,18 @@ int main()
                 }
             }
             //消完了QAQ
+            for (int j = 0; j < v.size(); j++) {
+                int now = v[j];
+                while (now <= N) {
+                    if ((now + 1) % 7 != 0 && now % 7 != 0)
+                        st.insert(now);
+                    now += v[j];
+                }
+            }
+            cout << st.size() << '\n';
+            st.clear();
         }
+        v.clear();
     }
 
     return 0;
