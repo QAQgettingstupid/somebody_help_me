06113cca4eaf68c8bff426763673101a67b06a87
QAQgettingstupid <venustsai211@gmail.com>
2024-01-11 18:17:53 +0800

    add a012&e510

---
+++ e510/e510.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+
+using namespace std;
+
+int main()
+{
+    // a/(1-r)無窮等比級數和公式, a->首項,r->公比(-1<r<1)
+    int S, N, i;
+    double p, ans;
+    cin >> S;
+    while (S--) {
+        cin >> N >> p >> i;
+        ans = (pow((1 - p), (i - 1)) * p) / (1 - pow((1 - p), N));
+        cout << fixed << setprecision(4) << ans << '\n';
+    }
+    return 0;
+}
