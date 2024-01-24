afb617f989d293547bd0409770586e8a7ed1f0b2
QAQgettingstupid <venustsai211@gmail.com>
2024-01-24 18:27:36 +0800

    add e566

---
+++ zerojudge是什麼能吃嗎/e566/e566.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    bool flag;
+    vector<int> num;
+    while (cin >> n >> m) {
+        if (m <= 1) {
+            cout << "Boring!\n";
+            continue;
+        }
+        flag = true;
+        num.push_back(n);
+        while (n != 1) {
+            if (n % m) {
+                cout << "Boring!\n";
+                flag = false;
+                break;
+            }
+            else {
+                num.push_back(n / m);
+                n /= m;
+            }
+        }
+        if (flag) {
+            cout << num[0];
+            for (int i = 1; i < num.size(); i++)
+                cout << " " << num[i];
+            cout << '\n';
+        }
+        num.clear();   
+    }
+
+    return 0;
+}
+
