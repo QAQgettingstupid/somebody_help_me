304c2b951931fdea20648a68c6a243bf1c3a784a
QAQgettingstupid <venustsai211@gmail.com>
2024-01-26 16:36:48 +0800

    又是你!!!!三角函數

---
+++ 你說我該把你丟哪阿AUA/Satellites/Satellites.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#define pi acos(0.0)*2.0
+using namespace std;
+
+int main()
+{
+    double s, a,ansc,ansa;
+    string unit;
+    while (cin >> s >> a >> unit) {
+        if (unit == "min")
+            a /= 60;
+        if (a > 180)
+            a = 360 - a;
+        ansc = (s + 6440) * (a / 180) * pi;
+        ansa = sqrt((2 * (s + 6440) * (s + 6440)) - (2 * (s + 6440) * (s + 6440) * cos((a / 180) * pi)));
+        //弧長
+        cout << fixed << setprecision(6) << ansc << " ";
+        //弦長
+        cout << ansa << '\n';
+    }
+
+    return 0;
+}
