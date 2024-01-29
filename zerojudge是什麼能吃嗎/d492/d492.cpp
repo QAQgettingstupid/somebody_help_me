082dcf37e17ad4cbf9508032dec8c84cb77e6bc9
QAQgettingstupid <venustsai211@gmail.com>
2024-01-29 15:42:53 +0800

    add d492

---
+++ zerojudge是什麼能吃嗎/d492/d492.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    double sum;
+    string s;
+    map<string, double> mp;
+    cin >> n;
+    getline(cin, s);
+    getline(cin, s);
+    while (n--) {
+        sum = 0;
+        do {
+            getline(cin, s);
+            if (s != ""){
+               sum++;
+               mp[s]++;
+            }
+        } while (s != "");
+        for (auto it : mp)
+            cout << fixed << setprecision(4) << it.first << " " << (it.second) / sum*100 << '\n';
+        mp.clear();
+    }
+
+    return 0;
+}
