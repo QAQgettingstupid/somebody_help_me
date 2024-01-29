64c97dc0cee405d8977ad6baa2af5a97d1a44f47
QAQgettingstupid <venustsai211@gmail.com>
2024-01-29 16:32:06 +0800

    add d387

---
+++ zerojudge是什麼能吃嗎/d387/d387.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int num[1000005] = { 0 };
+
+int main()
+{
+    int n, rnum, now;//0->質數 1->非質數 2->反質數 到1000000!!!!!
+    for (int i = 2; i < 1000000; i++) {
+        if(!num[i])
+            for (int j = i*2; j < 1000000; j += i)
+                num[j] = 1;
+    }
+    for (int i = 2; i < 1000000; i++) {
+        if (!num[i]){
+            rnum = 0;
+            now = i;
+            while (now) {
+                rnum *= 10;
+                rnum += now % 10;
+                now /= 10;
+            }
+            if (i != rnum && (num[rnum] == 0 || num[rnum] == 2))
+                num[i] = 2;
+        }
+    }
+    while (cin >> n) {
+        if (!num[n])
+            cout << n << " is prime.\n";
+        else if(num[n]==1)
+            cout << n << " is not prime.\n";
+        else
+            cout << n << " is emirp.\n";
+    }
+
+    return 0;
+}
+
