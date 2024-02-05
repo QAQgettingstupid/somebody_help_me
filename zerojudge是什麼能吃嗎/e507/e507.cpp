0c83b7c24fa5574368f5183648c9e2929753936a
QAQgettingstupid <venustsai211@gmail.com>
2024-02-05 16:29:58 +0800

    add e507

---
+++ zerojudge是什麼能吃嗎/e507/e507.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Min;
+    string s1, s2;
+    while (cin >> s1 >> s2) {
+        int abc[2][26] = { 0 };
+        for (int i = 0; i < s1.size(); i++)
+            abc[0][s1[i]-'a']++;
+        for (int i = 0; i < s2.size(); i++)
+            abc[1][s2[i] - 'a']++;
+        for (int i = 0; i < 26; i++) {
+            Min = min(abc[0][i], abc[1][i]);
+            for (int j = 0; j < Min; j++) {
+                char ans = 'a';
+                ans += i;
+                cout << ans;
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
+
