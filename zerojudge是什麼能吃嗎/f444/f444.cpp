33ae770d731c2d93104ee90d86867c7b3c528238
QAQgettingstupid <venustsai211@gmail.com>
2024-02-07 15:45:31 +0800

    add f444

---
+++ zerojudge是什麼能吃嗎/f444/f444.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int x,ans;
+    string s,an;
+    vector<int> num;
+    while (cin >> x) {
+        ans = 0;
+        getline(cin, s);
+        getline(cin, s);
+        stringstream ss(s);
+        while (ss >> an)
+            num.push_back(stoi(an));
+        reverse(num.begin(), num.end());
+        if (num.size() > 1)
+            for (int i = 1; i < num.size(); i++) {
+                num[i - 1] = num[i] * i;
+                ans += num[i - 1] * pow(x, i - 1);
+            }
+        else
+            ans = 0;
+        cout << ans << '\n';
+        num.clear();
+    }
+
+    return 0;
+}
+
