70d3e4af68c0c488bbb48e2d396560cd54e981ab
QAQgettingstupid <venustsai211@gmail.com>
2024-02-21 17:47:57 +0800

    add e516

---
+++ zerojudge是什麼能吃嗎/e516/e516.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,step,top;//east->0 south->1 west->2 north->3
+    int dice[7][4] = { 0,0,0,0,
+                       3,2,4,5,
+                       3,6,4,1,
+                       6,2,1,5,
+                       1,2,6,5,
+                       3,1,4,6,
+                       3,5,4,2, };
+    string s;
+    while (cin >> n) {
+        if (!n)
+            break;
+        top = 1;
+        while (n--) {
+            cin >> s;
+            if (s == "east")
+                step = 0;
+            else if(s == "south")
+                step = 1;
+            else if(s == "west")
+                step = 2;
+            else
+                step = 3;
+            top = dice[top][step];
+            cout << top;
+        }
+        cout << top << '\n';
+    }
+    
+
+    return 0;
+}
---
+++ 作業不好玩/n階層(50位數)/n階層(50位數).cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    while (1) {
+        cout << "please enter a number: ";
+        cin >> num;
+        int ans[55] = { 0 }, flag = 1, count; // flag->陣列ans目前用到的位數 
+        ans[0] = 1;                           // count->每一層運算用到的位數
+
+        for (int i = 2; i <= num; i++) {// 1*任何數=自己，故從2開始算
+            int sum = 0;
+            count = flag;
+            for (int j = 0; j < flag; j++) {//與目前陣列ans已有的位數相乘
+                int now = sum + ans[j] * i; 
+                ans[j] = now % 10;
+                sum = now / 10;
+            }
+
+            while (sum) {//將剩餘超出目前flag位數的sum另外記錄在陣列ans中
+                ans[count] = sum % 10;
+                count++;
+                sum /= 10;
+            }
+            flag = max(count, flag); //更新flag大小，取count和原flag中較大的
+            //max(a,b) -> 回傳a和b中較大的值
+        }
+        
+        for (int i = flag - 1; i >= 0; i--)
+            cout << ans[i];
+        cout << '\n';
+    }
+
+    return 0;
+}
+
