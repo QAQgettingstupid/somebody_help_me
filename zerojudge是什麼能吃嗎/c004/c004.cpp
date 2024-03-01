d502b6791b2db778dc4daf77288d897979c9fee0
QAQgettingstupid <venustsai211@gmail.com>
2024-03-01 17:33:24 +0800

    new

---
+++ zerojudge是什麼能吃嗎/c004/c004.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, s, d,big,small;
+    cin >> n;
+    while (n--) {
+        cin >> s >> d;
+        if (d > s || (s - d) % 2)
+            cout << "impossible\n";
+        else {
+            big = (s - d) / 2 + d;
+            small = (s - d) / 2;
+            cout << big << " " << small << '\n';
+        }
+    }
+
+    return 0;
+}
