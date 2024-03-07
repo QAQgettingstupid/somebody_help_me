1722a22aa382c88bd779fa555dc3e704d4e52e70
QAQgettingstupid <venustsai211@gmail.com>
2024-03-07 18:38:54 +0800

    linked list

---
+++ 作業不好玩/add_ from_end/add_ from_end.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class node {
+private:
+    int number;
+    node* next;
+
+public:
+    void set_number(int a) {
+        number = a;
+    }
+    void set_next(node* a) {
+        next = a;
+    }
+    int get_number() {
+        return number;
+    }
+    node* get_next() {
+        return next;
+    }
+
+};
+
+void printlist(node* p) {
+    cout << "head->";
+    while (p) {
+        cout << p->get_number() << "->";
+        p = p->get_next();
+    }
+    cout << "||\n";
+}
+
+int main()
+{
+    node* head= NULL;
+    node* p;
+    node* end = head;
+    int n;
+    while (1) {
+        cout << "Please enter a number:\n";
+        cin >> n;
+        if (!n)
+            break;
+        if (!head) {
+            head = new node;
+            head->set_number(n);
+            head->set_next(NULL);
+            end = head;
+        }
+        else {
+            p = new node;
+            end->set_next(p);
+            p->set_number(n);
+            p->set_next(NULL);
+            end = p;
+        }
+    }
+    printlist(head);
+
+    return 0;
+}
---
+++ 作業不好玩/add_from_front/add_from_front.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class node {
+private:
+    int number;
+    node* next;
+
+public:
+    void set_number(int a) {
+        number = a;
+    }
+    void set_next(node* a) {
+        next = a;
+    }
+    int get_number() {
+        return number;
+    }
+    node* get_next() {
+        return next;
+    }
+
+};
+
+void printlist(node* p) {
+    cout << "head->";
+    while (p) {
+        cout << p->get_number() << "->";
+        p = p->get_next();
+    }
+    cout << "||\n";
+}
+
+int main()
+{
+    node* head = NULL;
+    node* p;
+    int n;
+    while (1) {
+        cout << "Please enter a number:\n";
+        cin >> n;
+        if (!n)
+            break;
+        p = new node;
+        p->set_number(n);
+        p->set_next(head);
+        head = p;
+    }
+    printlist(head);
+
+    return 0;
+}
