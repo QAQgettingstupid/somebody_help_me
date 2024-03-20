cce80f3f6007403028a38528721dcd34a92a0b56
QAQgettingstupid <venustsai211@gmail.com>
2024-03-20 18:15:54 +0800

    weeeee~

--- 作業不好玩/sorted_and_delete/sorted_and_delete.cpp
+++ 作業不好玩/sorted_and_delete/sorted_and_delete.cpp
@@ -19,6 +19,12 @@ public:
     node* get_next() {
         return next;
     }
+    int operator+(node p) {
+        return number + p.get_number();
+    }
+    node(int a = 0) {
+        number = a;
+    }
 };
 
 int menu() {
@@ -26,7 +32,8 @@ int menu() {
     cout << "0 離開:\n";
     cout << "1 新增:\n";
     cout << "2 刪除:\n";
-    cout << "3 檢視串列:\n\n";
+    cout << "3 檢視串列:\n";
+    cout << "4 檢視串列總和:\n\n";
     cout << "請從中選一個:\n";
     cin >> n;
     cout << '\n';
@@ -71,20 +78,25 @@ void enter(node*& head) {
 
 }
 
-void Delete(node*& head) { //你怪怪der
+void Delete(node*& head) { 
     if (!head)
-        cout << "此串列為空\n";
+        cout << "此串列為空\n\n";
     else {
         int n;
+        node* temp;
         cout << "請輸入要刪除的數字:\n";
         cin >> n;
         while (head && head->get_number() == n) {
+            temp = head;
             head = head->get_next();
+            delete temp;
         }
         node* find = head;
         while (find && find->get_next()) {
             if (find->get_next()->get_number() == n) {
+                temp = find->get_next();
                 find->set_next(find->get_next()->get_next());
+                delete temp;
             }
             else
                 find = find->get_next();
@@ -93,13 +105,28 @@ void Delete(node*& head) { //你怪怪der
 
 }
 
+void add(node* head) {
+    if (!head)
+        cout << "串列數字和: " << 0 << '\n';
+    else {
+        node* sum = new node;
+        sum->set_number(0);
+        while (head) {
+            sum->set_number(*head + *sum);
+            head = head->get_next();
+        }
+        cout << "串列數字和: " << sum->get_number() << '\n';
+        delete sum;
+    }
+}
+
 void print(node* head) {
     cout << "head->";
     while (head) {
         cout << head->get_number() << "->";
         head = head->get_next();
     }
-    cout << "||\n";
+    cout << "||\n\n";
 }
 
 int main()
@@ -122,10 +149,12 @@ int main()
         case 3:
             print(head);
             break;
+        case 4:
+            add(head);
+            break;
         default:
             cout << "錯誤...請再輸入一次\n";
         }
     } while (n);
     return 0;
 }
-
