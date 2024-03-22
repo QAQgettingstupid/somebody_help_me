209c81a1e963545dfd336dd0721f214ee0373bc0
QAQgettingstupid <venustsai211@gmail.com>
2024-03-22 13:20:46 +0800

    ??

---
+++ 實驗場/class/class.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class node {
+    private:
+        int number;//成員
+        node* next;
+    public:
+        int get_number();//函式功能
+
+};
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
+
