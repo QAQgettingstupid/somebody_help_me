94a9bbf0363d462388ec2afb2d524e22ebb621f2
QAQgettingstupid <venustsai211@gmail.com>
2024-03-27 20:43:56 +0800

    new

---
+++ 作業不好玩/new_node/new_node.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class node {
+private:
+    int number, prg, cmp;
+    char name[10];
+    node* next;
+public:
+    void set_number(int a) {
+        number = a;
+    }
+    void set_prg() {
+        int a;
+        cout << "Please enter prg score:\n";
+        cin >> a;
+        prg = a;
+    }
+    void set_cmp() {
+        int a;
+        cout << "Please enter cmp score:\n";
+        cin >> a;
+        cmp = a;
+    }
+    void set_name() {
+        cout << "Please enter name:\n";
+        cin >> name;
+    }
+    void set_next(node* p) {
+        next = p;
+    }
+    int get_number() {
+        return number;
+    }
+    node* get_next() {
+        return next;
+    }
+
+    //輸出坨
+
+};
+
+void print(node* head) {
+    cout << "head->";
+    while (head) {
+        cout << head->get_number() << "->";
+        head = head->get_next();
+    }
+    cout << "||\n";
+}
+
+int main()
+{
+    node* head = NULL;
+    node* mid;
+    node* find;
+    node* p;
+    int n;
+    while (1) {
+        cout << "Please enter number:\n";
+        cin >> n;
+        if (n <= 0)
+            break;
+        if (!head) {
+            head = new node;
+            head->set_number(n);
+            head->set_prg();
+            head->set_cmp();
+            head->set_name();
+            head->set_next(NULL);
+        }
+        else {
+            bool flag = false;
+            p = new node;
+            p->set_number(n);
+            if (head->get_number() >= n) {
+                if (head->get_number() == n)
+                    flag = true;
+                else {
+                    p->set_next(head);
+                    p->set_prg();
+                    p->set_cmp();
+                    p->set_name();
+                    head = p;
+                }
+            }
+            else {
+                find = head;
+                while (find->get_next()) {
+                    if ((find->get_next())->get_number() >= n) {
+                        if (find->get_next()->get_number() == n) {
+                            flag = true;
+                            break;
+                        }
+                        else {
+                            mid = find->get_next();
+                            find->set_next(p);
+                            p->set_next(mid);
+                            p->set_prg();
+                            p->set_cmp();
+                            p->set_name();
+                            break;
+                        }
+                    }
+                    find = find->get_next();
+                }
+                if (!flag && !(find->get_next())) {
+                    find->set_next(p);
+                    p->set_prg();
+                    p->set_cmp();
+                    p->set_name();
+                    p->set_next(NULL);
+
+                }
+            }
+            if (flag) {
+                cout << "same no exist\n";
+            }
+        }
+    }
+    print(head);
+
+    return 0;
+}
