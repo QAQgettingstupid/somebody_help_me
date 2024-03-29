f5d6afd8905864f5a32245d15734c045d522cad5
QAQgettingstupid <venustsai211@gmail.com>
2024-03-29 17:04:01 +0800

    weeeee

--- 作業不好玩/new_node/new_node.cpp
+++ 作業不好玩/new_node/new_node.cpp
@@ -8,38 +8,67 @@ private:
     char name[10];
     node* next;
 public:
-    void set_number(int a) {
-        number = a;
-    }
-    void set_prg() {
-        int a;
-        cout << "Please enter prg score:\n";
-        cin >> a;
-        prg = a;
-    }
-    void set_cmp() {
-        int a;
-        cout << "Please enter cmp score:\n";
-        cin >> a;
-        cmp = a;
-    }
-    void set_name() {
-        cout << "Please enter name:\n";
-        cin >> name;
-    }
-    void set_next(node* p) {
-        next = p;
-    }
-    int get_number() {
-        return number;
-    }
-    node* get_next() {
-        return next;
-    }
-
-    //輸出坨
+    //輸入
+    void set_number(int a);
+    void set_prg();
+    void set_cmp();
+    void set_name();
+    void set_next(node* p);
+    //輸出
+    int get_number();
+    int get_prg();
+    int get_cmp();
+    node* get_next();
+    char* get_name();
 
 };
+
+void node::set_number(int a) {
+    number = a;
+}
+
+void node::set_prg() {
+    int a;
+    cout << "Please enter prg score:\n";
+    cin >> a;
+    prg = a;
+}
+
+void node::set_cmp() {
+    int a;
+    cout << "Please enter cmp score:\n";
+    cin >> a;
+    cmp = a;
+}
+
+void node::set_name() {
+    cout << "Please enter name:\n";
+    cin >> name;
+}
+
+void node::set_next(node* p) {
+    next = p;
+}
+
+int node::get_number() {
+    return number;
+}
+
+int node::get_prg() {
+    return prg;
+}
+
+int node::get_cmp() {
+    return cmp;
+}
+
+node* node::get_next() {
+    return next;
+}
+
+char* node::get_name() {
+    return name;
+}
 
 void print(node* head) {
     cout << "head->";
@@ -50,8 +79,7 @@ void print(node* head) {
     cout << "||\n";
 }
 
-int main()
-{
+int main(){
     node* head = NULL;
     node* mid;
     node* find;
@@ -87,31 +115,25 @@ int main()
             }
             else {
                 find = head;
-                while (find->get_next()) {
-                    if ((find->get_next())->get_number() >= n) {
-                        if (find->get_next()->get_number() == n) {
-                            flag = true;
-                            break;
-                        }
-                        else {
-                            mid = find->get_next();
-                            find->set_next(p);
-                            p->set_next(mid);
-                            p->set_prg();
-                            p->set_cmp();
-                            p->set_name();
-                            break;
-                        }
-                    }
+                while (find->get_next() && find->get_next()->get_number() < n) {
                     find = find->get_next();
                 }
-                if (!flag && !(find->get_next())) {
+                if (!(find->get_next())) {
                     find->set_next(p);
                     p->set_prg();
                     p->set_cmp();
                     p->set_name();
                     p->set_next(NULL);
-
+                }
+                else if (find->get_next()->get_number() == n)
+                    flag = true;
+                else {
+                    mid = find->get_next();
+                    find->set_next(p);
+                    p->set_next(mid);
+                    p->set_prg();
+                    p->set_cmp();
+                    p->set_name();
                 }
             }
             if (flag) {
