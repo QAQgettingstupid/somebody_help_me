4cd5d993a837f3cd7886a7148679cc00c61af446
QAQgettingstupid <venustsai211@gmail.com>
2024-05-03 13:39:43 +0800

    weeeeeeee

---
+++ 作業不好玩/virtual_inheritance/virtual_inheritance.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class A {
+protected:
+    int a;
+public:
+    A() {
+        a = 6;
+    }
+};
+
+class B :virtual public A {
+public:
+    void writea(int x);
+    int reada();
+};
+
+class C :virtual public A {
+
+};
+
+class D :public B, public C {
+
+};
+
+void B::writea(int x) {
+    a = x;
+}
+
+int B::reada() {
+    return a;
+}
+
+int main()
+{
+    D x;
+    cout << x.reada() << '\n';
+    x.writea(5);
+    cout << x.reada() << '\n';
+
+    return 0;
+}
+
