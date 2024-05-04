68cdeba7e8ab881bb6886f851135962726cc17a8
QAQgettingstupid <venustsai211@gmail.com>
2024-05-04 18:11:53 +0800

    我好智障QAQ

---
+++ zerojudge是什麼能吃嗎/d186/d186.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b, sq;
+    while (cin >> a >> b) {
+        int count = 0;
+        if (!(a + b))
+            break;
+        for (int i = a; i <= b; i++) {
+            sq = floor(sqrt(i));
+            if (sq * sq == i)
+                count++;
+        }
+        cout << count << '\n';
+    }
+
+    return 0;
+}
---
+++ zerojudge是什麼能吃嗎/e513/e513.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int t, n, count = 1;
+    string s;
+    cin >> t;
+    while (t--) {
+        bool flag = true;
+        long long int sqr[100][100];
+        cin >> s >> s >> n;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> sqr[i][j];
+                if (sqr[i][j] < 0)
+                    flag = false;
+            }
+        }
+        if (flag) {
+            for (int i = 0; i <= n / 2; i++) {
+                for (int j = 0; j < n; j++)
+                    if (sqr[i][j] != sqr[n - 1 - i][n - 1 - j])
+                        flag = false;
+            }
+        }
+        cout << "Test #" << count << ": ";
+        if (flag)
+            cout << "Symmetric.\n";
+        else
+            cout << "Non-symmetric.\n";
+        count++;
+    }
+
+    return 0;
+}
