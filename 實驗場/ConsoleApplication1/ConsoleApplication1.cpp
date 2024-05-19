02119df0eead2bc8417a12edabde2a72940b98e0
QAQgettingstupid <venustsai211@gmail.com>
2024-05-19 14:05:01 +0800

    東西好多

---
+++ 實驗場/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    int* head = NULL;
+    int* b = head;
+    head= &a;
+    b = head;
+    cout << *b;
+
+    return 0;
+}
