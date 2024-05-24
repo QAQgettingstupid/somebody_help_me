90bffbe3918358d45dca6fd32ca4fbbfecae8d23
QAQgettingstupid <venustsai211@gmail.com>
2024-05-24 17:05:33 +0800

    蛤啊?

---
+++ 個人愉悅用/step/step.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int a[5][5];
+    int x, y;
+    stack<int> s;
+    while (cin >> x >> y) {
+        if (!x || !y)
+            break;
+        a[x][y] = 1;
+    }
+
+
+
+    return 0;
+}
+
