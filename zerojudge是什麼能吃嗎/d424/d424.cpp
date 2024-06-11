a801fea5452d3ae212034e7a5056c345dfb4dc80
QAQgettingstupid <venustsai211@gmail.com>
2024-06-11 23:44:30 +0800

    ??

---
+++ zerojudge是什麼能吃嗎/d424/d424.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int high[100010] = { 0 };
+    int left,right,tall,end = 0;
+    while (cin >> left >> tall >> right) {
+        if (!(left + tall + right))
+            break;
+        if (right > end)
+            end = right;
+        for (int i = left; i <= right; i++) {
+            if (tall > high[i])
+                high[i] = tall;
+        }
+    }
+    //印出來的順序怪怪的
+    int front = high[1];
+    cout << 1 << " " << high[1] << " ";
+    for (int i = 2; i <= end; i++) {
+        if (high[i] != front) {
+            front = high[i];
+            if(!high[i])
+                cout << i-1 << " " << high[i] << " ";
+            else
+                cout << i << " " << high[i] << " ";
+        }
+    }
+    cout << end << " " << 0;
+    return 0;
+}
---
+++ zerojudge是什麼能吃嗎/f709/f709.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T, month, date, ans;
+    int doom[12] = { 10,21,7,4,9,6,11,8,5,10,7,12 };
+    string day[7] = { "Monday\n","Tuesday\n","Wednesday\n","Thursday\n","Friday\n","Saturday\n","Sunday\n" };
+    cin >> T;
+    while (T--) {
+        cin >> month >> date;
+        ans = (date - doom[month - 1]) % 7;
+        if (ans >= 0)
+            cout << day[ans];
+        else
+            cout << day[ans + 7];
+    }
+
+    return 0;
+}
