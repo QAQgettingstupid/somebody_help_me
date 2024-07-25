07cb348c85a583c8d46d64a923f833870a8c7bd6
QAQgettingstupid <venustsai211@gmail.com>
2024-07-25 16:56:01 +0800

    ?

---
+++ zerojudge是什麼能吃嗎/e569/e569.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <cctype>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector <string> list;
+    string s, temp;
+    string word = "";
+    int num = 0;
+    while (getline(cin, s)) {
+        if (s == "0")
+            break;
+        for (int i = 0; i < s.size(); i++) {
+
+            if (isalpha(s[i])) {
+                word += s[i];
+            }
+            else if (isdigit(s[i])) {
+                num *= 10;
+                num += s[i]-'0';
+            }
+            else {
+                if (word != "") {
+                    list.push_back(word);
+                    cout << word;
+                    word = "";
+                }
+                if (num) {
+                    cout << *(list.end() - num);
+                    temp = *(list.end() - num);
+                    if (num > 1) {
+                        list.push_back(temp);
+                        for (auto it = list.begin(); it < list.end(); it++) {
+                            if (*it == temp) {
+                                list.erase(it);
+                                break;
+                            }
+                        }
+                    }
+                    num = 0;
+                }
+                cout << s[i];
+            }
+        }
+        if (word != "") {
+            list.push_back(word);
+            cout << word;
+            word = "";
+        }
+        if (num) {
+            cout << *(list.end() - num);
+            temp = *(list.end() - num);
+            if (num > 1) {
+                list.push_back(temp);
+                for (auto it = list.begin(); it < list.end(); it++) {
+                    if (*it == temp) {
+                        list.erase(it);
+                        break;
+                    }
+                }
+            }
+            num = 0;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+
