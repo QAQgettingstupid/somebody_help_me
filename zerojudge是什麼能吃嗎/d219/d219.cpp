4d2c979a8f9121a8320a7313b6d1ffb57126712a
QAQgettingstupid <venustsai211@gmail.com>
2024-07-31 15:07:02 +0800

    我就還沒學快速幕&mod運算嘛

---
+++ zerojudge是什麼能吃嗎/d219/d219.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int B, P, M, base, ans;
+
+    while (cin >> B >> P >> M) {//快速幕&mod運算
+        ans = 1;
+        base = B % M;
+        while (P) {
+            if (P % 2) {
+                ans = (ans * base) % M;
+            }
+            P /= 2;
+            base = (base * base) % M;
+        }
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
+
