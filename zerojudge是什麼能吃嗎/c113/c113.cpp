3be8072157f7c83883ba45401bffb6074f7164ba
QAQgettingstupid <venustsai211@gmail.com>
2024-08-01 17:45:26 +0800

    是精度運算的鍋QAQ

--- zerojudge是什麼能吃嗎/c113/c113.cpp
+++ zerojudge是什麼能吃嗎/c113/c113.cpp
@@ -1,31 +1,72 @@
 ﻿#include <iostream>
 #include <iomanip>
 
+#define INF 1e9
+
 using namespace std;
+
+struct Line {
+    double x1, x2, y1, y2;
+    double m, b;//b -> y截距
+};
+
+void write_line(Line& line) {
+
+    if (line.x1 == line.x2) {
+        line.m = INF;
+        line.b = INF;
+    }
+    else {
+        line.m = (line.y2 - line.y1) / (line.x2 - line.x1);
+        line.b = line.y1 - line.m * line.x1;
+    }
+}
 
 int main()
 {
     int N;
-    double x1, x2, x3, x4, y1, y2, y3, y4;
-    double m1, m2, m3, b1, b2; //b -> y截距
+    double point_x, point_y;
+    Line l1, l2, l3;
     cout << "INTERSECTING LINES OUTPUT\n";
     cin >> N;
 
-    while (N--) { //鉛垂線斜率會出事,待修www
-        cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
-        m1 = (y2 - y1) / (x2 - x1);
-        m2 = (y4 - y3) / (x4 - x3);
-        m3 = (y2 - y1) / (x4 - x3);
-        if (m1 == m2 && m1 == m3)
+    while (N--) { 
+
+        cin >> l1.x1 >> l1.y1 >> l1.x2 >> l1.y2 >> l2.x1 >> l2.y1 >> l2.x2 >> l2.y2;
+
+        l3.x1 = l1.x1;
+        l3.x2 = l2.x1;
+        l3.y1 = l1.y1;
+        l3.y2 = l2.y1;
+        write_line(l1);
+        write_line(l2);
+        write_line(l3);
+
+        if (l1.m == l2.m && l1.m == l3.m)
             cout << "LINE\n";
-        else if (m1 == m2)
+        else if (l1.m == l2.m)
             cout << "NONE\n";
         else {
-            double point_x, point_y;
-            b1 = y1 + (-x1) * 3;
-            b2 = y3 + (-x3) * 3;
-            point_x = -(b2 - b1) / (m2 - m1);
-            point_y = m1 * point_x + b1;
+            if (l1.m == 0 && l2.m== INF) {
+                point_x = l2.x1;
+                point_y = l1.y1;
+            }
+            else if (l2.m == 0 && l1.m == INF) {
+                point_x = l1.x1;
+                point_y = l2.y1;
+            }
+            else if (l1.m == INF) {
+                point_x = l1.x1;
+                point_y = l2.y1 + (l1.x1 - l2.x1) * l2.m;
+            }
+            else if (l2.m == INF) {
+                point_x = l2.x1;
+                point_y = l1.y1 + (l2.x1 - l1.x1) * l1.m;
+            }
+            else {
+                point_x = -(l2.b - l1.b) / (l2.m - l1.m);
+                point_y = l1.m * point_x + l1.b;
+            }
             cout << "POINT " << fixed << setprecision(2) << point_x << " " << setprecision(2) << point_y << '\n';
         }
     }
