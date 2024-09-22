f2153318ec7e8b8a1c4b3c5b0e75b3bceb0fa624
QAQgettingstupid <venustsai211@gmail.com>
2024-09-23 00:36:36 +0800

    ?

--- 實驗場/first/src/test.java
+++ 實驗場/first/src/test.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
+class parent {
+
+    public void show(){
+        System.out.println("from parent\n");  
+    }
+}
+
+class child extends parent{
+
+    public void show(){
+        System.out.println("from child\n");  
+    }
+}
+
 public class test {
 
     public void Add(double a, double b) {
@@ -19,11 +33,11 @@ public class test {
         double a = scanner.nextDouble(), b = scanner.nextDouble();
         scanner.close();
         test first = new test();
-        first.Add(a, b);
-        test.add(a, b);
 
-        String s1 = "GeeksforGeeks";
-        System.out.println("s1 = " + s1);
+        parent test1 = new parent();
+        parent test2 = new child();
 
+        test1.show();
+        test2.show();
     }
 }
