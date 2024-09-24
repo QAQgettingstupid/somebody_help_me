fd9318799b7f1ac5050c18bdb545154c4723e5ad
QAQgettingstupid <venustsai211@gmail.com>
2024-09-24 18:36:07 +0800

    ?

--- 實驗場/for_java/F3733-master/範例/Ch1/Ch1_1/Ch1_1.java
+++
@@ -1,6 +0,0 @@
-// Ch1_1, 第一個Java程式
-public class Ch1_1{
-    public static void main(String[] args){
-        System.out.println("Hello Java!"); 
-    }
-}
--- 實驗場/for_java/F3733-master/範例/Ch1/Ch1_2/Ch1_2.java
+++
@@ -1,7 +0,0 @@
-// Ch1_2, 第二個Java程式
-public class Ch1_2{
-    public static void main(String[] args){
-        System.out.println("Hello Java!"); 
-        System.out.println("My second Java program"); 
-    }
-}
