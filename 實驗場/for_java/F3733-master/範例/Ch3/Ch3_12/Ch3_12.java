083c89892c0e644de13c4fead539bd1c978a51ed
藤井夕夜 <135799789+QAQgettingstupid@users.noreply.github.com>
2024-10-01 15:22:27 +0800

    ?

--- 實驗場/for_java/F3733-master/範例/Ch3/Ch3_12/Ch3_12.java
+++ 實驗場/for_java/F3733-master/範例/Ch3/Ch3_12/Ch3_12.java
@@ -5,13 +5,12 @@ public class Ch3_12{
         float ft=3.14f;
         double db=567.1234;
 
-        /*System.out.printf("bt=%c\n",bt);    		// 列印字元
+        System.out.printf("bt=%c\n",bt);    		// 列印字元
         System.out.printf("bt=%+-5d\n",bt);		// flag "-" 和 "0" 會衝突
         System.out.printf("oct=%o, hex=%x\n",bt, bt);  // 以不同進位數列印
         System.out.printf("ft=%7.4f\n",ft); 		// 列印浮點數
-        System.out.printf("db=%f\n",db); 		// 列印倍精度浮點數
-        */
-        System.out.printf("oct=%o, hex=%x\n",bt, bt);
+        System.out.printf("db=%f\n\n",db); 		// 列印倍精度浮點數
+        
         System.out.printf("%-4c %-4c %-4c %-4c %-4c %-4c %-4c\n\n",'一','二','三','四','五','六','日');
         System.out.printf("%-5d %-5d %-5d %-5d %-5d %-5d %-5d\n",30,1,2,3,4,5,6);
     }
