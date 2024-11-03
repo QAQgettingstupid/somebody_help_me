71585377bce20077006116d83bfa5d5c35a087af
QAQgettingstupid <venustsai211@gmail.com>
2024-11-03 13:37:55 +0800

    ?

---
+++ 作業不好玩/java/H3_111216019/src/H3_111216019.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class H3_111216019 {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        while (scn.hasNext()) {
+
+            // Random
+            Random rand = new Random();
+            int num = rand.nextInt(10);
+            System.out.println(num);
+
+            int input = scn.nextInt();
+
+        }
+        scn.close();
+    }
+}
