bf0b747ad8e9e1f656fe94a16d53e50d45e24b49
藤井夕夜 <135799789+QAQgettingstupid@users.noreply.github.com>
2024-11-12 15:05:14 +0800

    ?

--- 操作手冊/範例說明用別刪!!!!!!!!!!!/for_java/F3733-master/範例/Ch13/Ch13_2/Ch13_2.java
+++ 操作手冊/範例說明用別刪!!!!!!!!!!!/for_java/F3733-master/範例/Ch13/Ch13_2/Ch13_2.java
@@ -3,9 +3,9 @@ public class Ch13_2{
     public static void main(String[] args){
         try{ 			// 檢查這個程式區塊的程式碼           
             int arr[]=new int[5];
-            arr[10]=7;
+            arr[10]=70000;
         }
-        catch(ArrayIndexOutOfBoundsException e){
+        catch(NumberFormatException e){
             System.out.println("index out of bound!!");
         }
         finally{   		// 這個區塊的程式碼一定會執行      
