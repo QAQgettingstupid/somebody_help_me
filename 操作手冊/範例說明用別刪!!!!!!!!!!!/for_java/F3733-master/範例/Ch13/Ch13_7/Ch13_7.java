9d55f6b5ef60670d548be2c38aaa07eb1dd3e31b
藤井夕夜 <135799789+QAQgettingstupid@users.noreply.github.com>
2024-11-18 18:32:18 +0800

    ?

--- 操作手冊/範例說明用別刪!!!!!!!!!!!/for_java/F3733-master/範例/Ch13/Ch13_7/Ch13_7.java
+++ 操作手冊/範例說明用別刪!!!!!!!!!!!/for_java/F3733-master/範例/Ch13/Ch13_7/Ch13_7.java
@@ -1,18 +1,18 @@
 // Ch13_7, 從不同類別內的函數拋出例外
-class Test{
-    public static void aaa(int a,int b) throws ArithmeticException{   
-        int c=a/b;
-        System.out.println(a+"/"+b+"="+c);  
-    }    
+class Test {
+    public static void aaa(int a, int b) /*throws ArithmeticException*/ {
+        int c = a / b;
+        System.out.println(a + "/" + b + "=" + c);
+    }
 }
 
-public class Ch13_7{
-    public static void main(String args[]){  
-        try{
-            Test.aaa(4,0);
-        }
-        catch(ArithmeticException e){
-            System.out.println(e+" throwed");
+public class Ch13_7 {
+    public static void main(String args[]){
+        Test.aaa(4, 0);
+        try {
+            // Test.aaa(4, 0);
+        } catch (ArithmeticException e) {
+            System.out.println(e + " throwed");
         }
     }
 }
