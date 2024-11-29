f5d809ede10d46ccc47539e07a725ea52c8d2756
QAQgettingstupid <venustsai211@gmail.com>
2024-11-29 17:57:15 +0800

    java 的 goto 意外地沒用

--- 作業不好玩/java/H5_111216019/src/H5_111216019.java
+++ 作業不好玩/java/H5_111216019/src/H5_111216019.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 //作者:蔡芳宇
 //學號:111216019
-//自評:20%+60%+20% (不支援負數運算)
+//自評:20%+60%+20%
 
 public class H5_111216019 {
     public static void main(String[] args) throws Exception {
@@ -17,75 +17,115 @@ public class H5_111216019 {
         priority.put('/', 2);
         priority.put('*', 2);
 
+        System.out.println("please enter expression: ");
+
         while (scn.hasNext()) {
-            String imput = scn.nextLine();
+            String input = scn.nextLine();
             Stack<Character> operator = new Stack<>();
             Stack<Integer> operand = new Stack<>();
             int index = 0, temp_num = 0;
-
-            // System.out.println(imput);
+            boolean negative = false, devide_0 = false;
 
             // 逐個讀取輸入字元
-            while (index < imput.length()) {
-                if (imput.charAt(index) <= '9' && imput.charAt(index) >= '0') {
+            while (index < input.length()) {
+                if (input.charAt(index) <= '9' && input.charAt(index) >= '0') {
                     temp_num *= 10;
-                    temp_num += imput.charAt(index) - '0';
-                } else if (imput.charAt(index) == '+' || imput.charAt(index) == '-' || imput.charAt(index) == '*'
-                        || imput.charAt(index) == '/' || imput.charAt(index) == '(' || imput.charAt(index) == ')') {
+                    temp_num += input.charAt(index) - '0';
+                } else if (input.charAt(index) == '+' || input.charAt(index) == '-' || input.charAt(index) == '*'
+                        || input.charAt(index) == '/' || input.charAt(index) == '(' || input.charAt(index) == ')') {
 
                     // 放入數字
                     if (temp_num != 0) {
-                        operand.push(temp_num);
+                        if (negative) {
+                            operand.push(temp_num * (-1));
+                            negative = false;
+                        } else
+                            operand.push(temp_num);
                         temp_num = 0;
                     }
 
+                    // 負號
+                    if (input.charAt(index) == '-'
+                            && (index == 0
+                                    || (index - 1 >= 0
+                                            && (input.charAt(index - 1) == '+' || input.charAt(index - 1) == '('
+                                                    || input.charAt(index - 1) == '*' || input.charAt(index - 1) == '-'
+                                                    || input.charAt(index - 1) == '/')))) {
+                        negative = true;
+                        index++;
+                        continue;
+                    }
+
                     // 左括弧(7+23)*(3-1)/(3+7)
-                    if (imput.charAt(index) == '(')
-                        operator.push(imput.charAt(index));
+                    if (input.charAt(index) == '(')
+                        operator.push(input.charAt(index));
 
                     // 右括弧
-                    else if (imput.charAt(index) == ')') {
+                    else if (input.charAt(index) == ')') {
 
                         // 對operator裡在'('之前的運算子進行運算
-                        while (operator.peek() != '(') {
-                            operand.push(count(operand, operator.peek()));
+                        while (!devide_0 && operator.peek() != '(') {
+                            try {
+                                operand.push(count(operand, operator.peek()));
+                            } catch (ArithmeticException e) {
+                                System.out.println("Division by zero is not allowed.");
+                                devide_0 = true;
+                                break;
+                            }
                             operator.pop();
                         }
                         operator.pop();
                     }
 
-                    // 都還沒實作,負數辨認也還沒搞
                     else {
 
                         // 輸入優先權小於等於stack top,先pop進行運算
-                        while (!operator.empty()
-                                && priority.get(operator.peek()) >= priority.get(imput.charAt(index))) {
-                            operand.push(count(operand, operator.peek()));
+                        while (!devide_0 && !operator.empty()
+                                && priority.get(operator.peek()) >= priority.get(input.charAt(index))) {
+                            try {
+                                operand.push(count(operand, operator.peek()));
+                            } catch (ArithmeticException e) {
+                                System.out.println("Division by zero is not allowed.");
+                                devide_0 = true;
+                                break;
+                            }
                             operator.pop();
                         }
-                        operator.push(imput.charAt(index));
+                        operator.push(input.charAt(index));
                     }
-                } else {
-                    index++;
-                    continue;
                 }
                 index++;
             }
             if (temp_num != 0)
-                operand.push(temp_num);
+                if (negative) {
+                    operand.push(temp_num * (-1));
+                    negative = false;
+                } else
+                    operand.push(temp_num);
 
             // 檢查是否還有運算子尚未運算
-            while (!operator.empty()) {
-                operand.push(count(operand, operator.peek()));
+            while (!devide_0 && !operator.empty()) {
+                try {
+                    operand.push(count(operand, operator.peek()));
+                } catch (ArithmeticException e) {
+                    System.out.println("Division by zero is not allowed.");
+                    devide_0 = true;
+                    break;
+                }
                 operator.pop();
             }
-            System.out.println("total=" + operand.peek());
-            operand.pop();
+
+            if (!devide_0) {
+                System.out.println(operand.peek());
+                operand.pop();
+            }
+            System.out.println();
+            System.out.println("please enter expression: ");
         }
     }
 
     // 運算函式實作
-    public static Integer count(Stack<Integer> s, Character c) {
+    public static Integer count(Stack<Integer> s, Character c) throws ArithmeticException {
         int result = 0;
         int back = s.peek();
         s.pop();
@@ -103,10 +143,7 @@ public class H5_111216019 {
                 result = front * back;
                 break;
             case '/':
-                if (back == 0)
-                    System.out.println("Division by zero is not allowed.");
-                else
-                    result = front / back;
+                result = front / back;
                 break;
         }
         return result;
