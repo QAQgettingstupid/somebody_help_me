1ea66ce98abb7ac0f0f5fbca51d29507bc773b5b
QAQgettingstupid <venustsai211@gmail.com>
2025-02-06 16:37:35 +0800

    槓 題目又沒寫好

--- zerojudge是什麼能吃嗎/c129/c129.cpp
+++ zerojudge是什麼能吃嗎/c129/c129.cpp
@@ -20,8 +20,13 @@ int main()
             for (int j = 1; j <= n; j++) {
                 cin >> ground[i][j];
                 if (ground[i][j] == '@')
-                    oil_place.insert(make_pair(m, n));
+                    oil_place.insert(make_pair(i, j));
             }
+
+        
+        /*pair<int, int> i;
+        for (auto i : oil_place)
+            cout << i.first << " " << i.second << "\n";*/
 
         //disconnected graph 反覆遍歷
         while (!oil_place.empty()) {
@@ -29,12 +34,47 @@ int main()
             //BFS
             oil.push(*oil_place.begin());
             oil_place.erase(oil_place.begin());
+            int size = 0;
             while (!oil.empty()) {
 
                 //上
-                if(oil_place.count(make_pair())
+                if (oil_place.count(make_pair(oil.front().first, oil.front().second + 1)))
+                    oil.push(make_pair(oil.front().first, oil.front().second + 1));
+
+                //下
+                if (oil_place.count(make_pair(oil.front().first, oil.front().second - 1)))
+                    oil.push(make_pair(oil.front().first, oil.front().second - 1));
+
+                //左
+                if (oil_place.count(make_pair(oil.front().first - 1, oil.front().second)))
+                    oil.push(make_pair(oil.front().first - 1, oil.front().second));
+
+                //右
+                if (oil_place.count(make_pair(oil.front().first + 1, oil.front().second)))
+                    oil.push(make_pair(oil.front().first + 1, oil.front().second));
+
+                //右上
+                if (oil_place.count(make_pair(oil.front().first + 1, oil.front().second + 1)))
+                    oil.push(make_pair(oil.front().first + 1, oil.front().second + 1));
+
+                //右下
+                if (oil_place.count(make_pair(oil.front().first + 1, oil.front().second - 1)))
+                    oil.push(make_pair(oil.front().first + 1, oil.front().second - 1));
+
+                //左上
+                if (oil_place.count(make_pair(oil.front().first - 1, oil.front().second + 1)))
+                    oil.push(make_pair(oil.front().first - 1, oil.front().second + 1));
+
+                //左下
+                if (oil_place.count(make_pair(oil.front().first - 1, oil.front().second - 1)))
+                    oil.push(make_pair(oil.front().first - 1, oil.front().second - 1));
+
+                size++;
+                oil_place.erase(oil.front());
+                oil.pop();
             }
-            ans++;
+            if (size)
+                ans++;
         }
 
         cout << ans << "\n";
