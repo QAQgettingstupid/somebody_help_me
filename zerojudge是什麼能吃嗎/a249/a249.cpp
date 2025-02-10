43f9c5758eaaeafed9c222557384addca103f3b1
QAQgettingstupid <venustsai211@gmail.com>
2025-02-10 18:30:47 +0800

    道理對了剩下就擺爛了www

--- zerojudge是什麼能吃嗎/a249/a249.cpp
+++ zerojudge是什麼能吃嗎/a249/a249.cpp
@@ -4,9 +4,25 @@ using namespace std;
 
 int main()
 {
-    int n, D, I;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, D, I, ans;
     cin >> n;
     while (n--) {
+        ans = 1;
+        cin >> D >> I;
 
+        //計算--> 球編號-1後轉二進位恰好為路徑, 1 -> 向右, 0 -> 向左
+        I--;
+        while (D > 1) {
+            if (I % 2)
+                ans = ans * 2 + 1;
+            else
+                ans *= 2;
+            I >>= 1;
+            D--;
+        }
+
+        cout << ans << '\n';
     }
 }
