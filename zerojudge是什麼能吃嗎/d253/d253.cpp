dc5c68720492ea1f7d294791d864b0013f863978
QAQgettingstupid <venustsai211@gmail.com>
2025-02-10 18:07:16 +0800

    為什麼算到動態規劃來了www

---
+++ zerojudge是什麼能吃嗎/d253/d253.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ways[7500] = { 0 };
+    int cents[] = { 1,5,10,25,50 };
+    ways[0] = 1;
+    for (int i = 0; i < 5; i++) {
+        for (int j = cents[i]; j <= 7489; j++)
+            ways[j] += ways[j - cents[i]];
+    }
+
+
+    int n;
+    while (cin >> n) {
+        cout << ways[n] << '\n';
+    }
+}
---
+++ zerojudge是什麼能吃嗎/e544/e544.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class sort_string {
+
+public:
+    string s;
+    int sequence;
+
+    void setsequence() {
+        sequence = 0;
+        int length = s.size();
+
+        for (int i = 0; i < length; i++)
+            for (int j = i + 1; j < length; j++)
+                if (s[i] > s[j])
+                    sequence++;
+    }
+};
+
+void sort(sort_string input[],int lines) {
+    for (int i = 0; i < lines; i++)
+        for (int j = 0; j < lines - i - 1; j++)
+            if (input[j].sequence > input[j + 1].sequence)
+                swap(input[j], input[j + 1]);
+}
+
+int main()
+{
+    int T, n, m;
+    cin >> T;
+    while (T--) {
+        cin >> n >> m;
+        sort_string input[110];
+        for (int i = 0; i < m; i++) {
+            cin >> input[i].s;
+            input[i].setsequence();
+        }
+
+        sort(input,m);
+
+        for (int i = 0; i < m; i++)
+            cout << input[i].s << '\n';
+
+        cout << '\n';
+    }
+
+}
