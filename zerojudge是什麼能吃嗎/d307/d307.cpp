5bf97c5e880f2a9ae1fb657e264cf6554a5b3d3c
QAQgettingstupid <venustsai211@gmail.com>
2025-02-18 18:56:41 +0800

    好簡單啊AUA

---
+++ zerojudge是什麼能吃嗎/d307/d307.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int prime[32768] = {0}; // 1 -> not prime, 0 -> prime
+    prime[0] = prime[1] = 1;
+    for (int i = 2; i < 32768; i++) {
+        if (!prime[i])
+            for (int j = i*2; j < 32768; j += i)
+                prime[j] = 1;
+    }
+    
+    int n, ans;
+    
+    while (cin >> n) {
+        if (!n)
+            break;
+
+        ans = 0;
+        for (int i = 2; i <= n / 2; i++)
+            if (!prime[i] && !prime[n - i])
+                ans++;
+
+        cout << ans << '\n';
+    }
+}
