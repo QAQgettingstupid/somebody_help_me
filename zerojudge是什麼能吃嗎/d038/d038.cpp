f31927cb6fcdeca9cd255df2615929482fdfce4b
藤井夕夜 <135799789+QAQgettingstupid@users.noreply.github.com>
2025-03-03 20:59:05 +0800

    老師對不起我忘了QAQ

---
+++ zerojudge是什麼能吃嗎/d038/d038.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int length[55];
+    length[1] = 1;
+    length[2] = 2;
+    for (int i = 3; i <= 50; i++)
+        length[i] = length[i - 1] + length[i - 2]; //考慮尾端方塊為直立或水平
+
+    int n;
+    while (cin >> n) {
+        if (!n)
+            break;
+        cout << length[n] << '\n';
+    }
+}
