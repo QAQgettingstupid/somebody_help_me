78a3f6744daacec490598fde982c659aada82b68
藤井夕夜 <135799789+QAQgettingstupid@users.noreply.github.com>
2025-05-02 15:25:59 +0800

    ?

---
+++ Arduino/sketch_mar14a/dot_line/dot_line.ino
@@ -0,0 +1,122 @@
+const byte LEFT1 = 8; //IN1
+const byte LEFT2 = 9; //IN2
+const byte LEFT_PWM = 10;
+//右馬達控制設定
+const byte RIGHT1 = 7; //IN3
+const byte RIGHT2 = 6; //IN4
+const byte RIGHT_PWM = 5;
+//設定PWM輸出值(代表的是車子的速度)
+byte rightspeed = 120;
+byte leftspeed =120;
+byte motorspeed = 90;
+int //count=0;
+
+void backward(){
+  digitalWrite(LEFT1, HIGH);
+  digitalWrite(LEFT2, LOW);
+  analogWrite(LEFT_PWM, leftspeed-40);
+  //右輪·因在小車上馬達安装方向左右兩個是相
+  digitalWrite(RIGHT1, LOW);
+  digitalWrite (RIGHT2, HIGH);
+  analogWrite(RIGHT_PWM, rightspeed-40);
+  /*count++;
+  if(count>30)
+    forward();*/
+}
+
+void forward(){ //
+  digitalWrite(LEFT1, LOW);
+  digitalWrite (LEFT2, HIGH);
+  analogWrite(LEFT_PWM, leftspeed);
+  digitalWrite(RIGHT1, HIGH);
+  digitalWrite(RIGHT2, LOW);
+  analogWrite(RIGHT_PWM, rightspeed);
+  //count=0;
+}
+
+void turnleft(){//左轉
+  //左輪不動,右輪動(速度為0)
+  analogWrite(LEFT_PWM, 0);
+  analogWrite(RIGHT_PWM, motorspeed);
+  digitalWrite(RIGHT1, HIGH);
+  digitalWrite (RIGHT2, LOW);
+
+}
+void bigturnleft(){//大左轉
+  //左輪不動,右輪動(速度為0)
+  analogWrite(LEFT_PWM, 0);
+  analogWrite(RIGHT_PWM, motorspeed+20);
+  digitalWrite(RIGHT1, HIGH);
+  digitalWrite (RIGHT2, LOW);
+}
+
+void turnright(){//右轉
+  //右輪不動,左輪動(速度為0)
+  analogWrite(LEFT_PWM, motorspeed);
+  analogWrite(RIGHT_PWM, 0);
+  digitalWrite(LEFT1, LOW);
+  digitalWrite(LEFT2, HIGH);
+}
+
+void bigturnright(){//大右轉
+  //右輪不動,左輪動(速度為0)
+  analogWrite(LEFT_PWM, motorspeed+20);
+  analogWrite(RIGHT_PWM, 0);
+  digitalWrite(LEFT1, LOW);
+  digitalWrite(LEFT2, HIGH);
+}
+void stopMotor() {
+  analogWrite(LEFT_PWM, 0);
+  analogWrite(RIGHT_PWM, 0);
+}
+
+void setup(){ 
+  //設定每一個PIN的模式
+  pinMode(LEFT1, OUTPUT);
+  pinMode(LEFT2, OUTPUT);
+  pinMode(LEFT_PWM, OUTPUT);
+  pinMode(RIGHT1, OUTPUT);
+  pinMode(RIGHT2, OUTPUT);
+  pinMode(RIGHT_PWM, OUTPUT);
+  Serial.begin(9600);
+
+
+  pinMode(4,INPUT); //中間红外線威測器
+  pinMode(11,INPUT);//右側红外線威測器
+  pinMode(12,INPUT);//左側红外線威測器
+}
+void loop() {
+
+  //Serial.print("right"); Serial.println(digitalRead(11));
+  Serial.print("left"); Serial.println(digitalRead(12));
+  //Serial.print("middle"); Serial.println(digitalRead(4));
+
+  //0-> 非黑線 1-> 黑線
+  // 空->
+
+  //正直走 010 111 101 
+  if((digitalRead(12)==0 && digitalRead(4)==1 && digitalRead(11)==0) || (digitalRead(12)==1 && digitalRead(4)==1 && digitalRead(11)==1) || (digitalRead(12)==1 && digitalRead(4)==0 && digitalRead(11)==1)){
+    forward();
+  }
+  //小右轉 011
+  if(digitalRead(12)==0 && digitalRead(4)==1 && digitalRead(11)==1){
+    turnright();
+  }
+  //大左轉 100
+  if(digitalRead(12)==1 && digitalRead(4)==0 && digitalRead(11)==0){
+    bigturnleft();
+    Serial.println("here!!!!!!");
+  }
+  //小左轉 110
+  if(digitalRead(12)==1 && digitalRead(4)==1 && digitalRead(11)==0){
+    turnleft();
+  }
+  //大右轉 001
+  if(digitalRead(12)==0 && digitalRead(4)==0 && digitalRead(11)==1){
+    bigturnright();
+  }
+  //停下 000
+  if(digitalRead(12)==0 && digitalRead(4)==0 && digitalRead(11)==0){
+    backward();
+  }
+}
