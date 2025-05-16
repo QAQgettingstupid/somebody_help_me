0125a1dcf9a04b5aa313b3df3e0db93f191685a7
藤井夕夜 <135799789+QAQgettingstupid@users.noreply.github.com>
2025-05-16 21:32:22 +0800

    ?

--- flutter/flutter_week1/lib/main.dart
+++ flutter/flutter_week1/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main(){
-  runApp(MaterialApp(home:myapp()));
+  runApp(MaterialApp(home:Myapp()));
 }
 
-class myapp extends StatelessWidget {
-  const myapp({super.key});
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
