a721235851affd5f88b34221216c248602ec366a
藤井夕夜 <venustsai211@gmail.com>
2025-09-15 18:12:38 +0800

    ?

---
+++ toolbox/挖書好難/regex/regex.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    string s;
+    while (getline(in, s)) {
+        s.insert(0, "<p>");
+        s += "</p>";
+        out << s << '\n';
+    }
+    //沒屁用www
+    in.close();
+    out.close();
+}
