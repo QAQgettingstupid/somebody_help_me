0630c8bc35165496843fc322aae6e557e07f3fae
藤井夕夜 <venustsai211@gmail.com>
2025-09-29 20:43:36 +0800

    ?

---
+++ zerojudge是什麼能吃嗎/c032/c032.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "PERFECTION OUTPUT\n";
+    
+    while (cin >> n) {
+        if (n == 0)
+            break;
+
+		int total = 0;
+        vector<int> ans;
+		ans.push_back(1);
+
+        for (int i = 2; i < sqrt(n); i++) {
+            if(n % i == 0) {
+                ans.push_back(i);
+                ans.push_back(n/i);
+			}
+        }
+        
+        for(auto it:ans)
+			total += it;
+        
+        cout << setw(5) << setfill(' ') << n << "  ";
+        
+        if (total < n || n == 1)
+            cout << "DEFICIENT\n";
+        else if (total == n)
+            cout << "PERFECT\n";
+        else
+            cout << "ABUNDANT\n";
+    }
+    cout << "\n";
+    cout << "END OF OUTPUT";
+}
